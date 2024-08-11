d56d40b1544476ba13fa39432ef7e61f7fc0f6b8
dhia336 <73760171+dhia336@users.noreply.github.com>
2024-08-11 10:48:32 +0200

    GUI initialization

---
+++ main.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+# window configs
+root = ctk.CTk()
+root.title("File Organizer")
+# title
+# choose folder
+# checkboxes (sort by [extention ,file type ,name ,date ,size....])
+# run button
+root.mainloop()
